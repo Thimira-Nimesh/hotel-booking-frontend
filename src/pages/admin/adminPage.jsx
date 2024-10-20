ba51c05017a9bfb163bb83ba82bc68eaa8c77d7e
tnimesh12345 <ranasin-im19079@stu.kln.ac.lk>
2024-10-21 02:18:12 +0530

    after add icons

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -2,24 +2,45 @@ import { Link, Route, Routes } from "react-router-dom";
 import Rooms from "./roomsPage";
 import Bookings from "./bookingsPage";
 import Categories from "./categoriesPage";
+import { CiBookmarkCheck } from "react-icons/ci";
+import {
+  BiCategory,
+  BiRoom,
+  BiUser,
+  BiFeedback,
+  BiImage,
+} from "react-icons/bi";
 
 export default function AdminPage() {
   return (
-    <div className="w-full h-[100vh] bg-red-400">
-      <div>
-        <Link className="mr-12" to={"/admin/rooms"}>
-          Rooms
-        </Link>
-        <Link className="mr-12" to={"/admin/bookings"}>
-          bookings
-        </Link>
-        <Link to={"/admin/categories"}>categories</Link>
+    <div className="w-full max-h-[100vh] overflow-hidden overflow-y-hidden flex">
+      <div className="w-[25%] bg-amber-900 h-[100vh] flex flex-col gap-6 p-4">
+        <div className="text-white text-[30px] flex items-center gap-4 hover:text-black hover:font-bold">
+          <CiBookmarkCheck />
+          <Link to={"/admin/bookings"}>Bookings</Link>
+        </div>
+        <div className="text-white text-[30px] flex items-center gap-4 hover:text-black hover:font-bold">
+          <BiCategory />
+          <Link to={"/admin/categories"}>Categories</Link>
+        </div>
+        <div className="text-white text-[30px] flex items-center gap-4 hover:text-black hover:font-bold">
+          <BiRoom />
+          <Link to={"/admin/rooms"}>Rooms</Link>
+        </div>
+        <div className="text-white text-[30px] flex items-center gap-4 hover:text-black hover:font-bold">
+          <BiUser />
+          <Link to={"/admin/users"}>Users</Link>
+        </div>
+        <div className="text-white text-[30px] flex items-center gap-4 hover:text-black hover:font-bold">
+          <BiFeedback />
+          <Link to={"/admin/feedbacks"}>Feedbacks</Link>
+        </div>
+        <div className="text-white text-[30px] flex items-center gap-4 hover:text-black hover:font-bold">
+          <BiImage />
+          <Link to={"/admin/galleryItem"}>Gallery Items</Link>
+        </div>
       </div>
-      <Routes path="/*">
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+      <div className="w-[80%] bg-blue-400 h-[100vh]"></div>
     </div>
   );
 }
