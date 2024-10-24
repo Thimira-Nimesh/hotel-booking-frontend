8c204cf5a2d82d0de930675034f9ac1e3b7492ce
tnimesh12345 <ranasin-im19079@stu.kln.ac.lk>
2024-10-24 23:20:24 +0530

    connect signup with backend

--- src/pages/signup/signUp.jsx
+++ src/pages/signup/signUp.jsx
@@ -1,7 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 // import LoginPages from "../login/loginfinalPage";
 
 export default function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [whatsapp, setWhatsapp] = useState();
+  const [phone, setPhone] = useState();
+  const [userType, setUserType] = useState("user");
+  const [emailVerified, setEmailVerified] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  function handleSubmit() {
+    axios
+      .post("http://localhost:5000/api/users", {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        whatsapp: whatsapp,
+        phone: phone,
+        userType: userType,
+        emailVerified: emailVerified,
+        disabled: disabled,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-[400px]">
@@ -9,82 +39,186 @@ export default function SignUp() {
           Register
         </h2>
 
-        <form>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-medium mb-2"
-              htmlFor="name"
-            >
-              Full Name
-            </label>
-            <input
-              id="name"
-              type="text"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your full name"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-medium mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your email"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-medium mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your password"
-            />
-          </div>
-
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-medium mb-2"
-              htmlFor="confirm-password"
-            >
-              Confirm Password
-            </label>
-            <input
-              id="confirm-password"
-              type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Confirm your password"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+        {/* First Name */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="firstName"
           >
-            Register
-          </button>
-        </form>
+            First Name
+          </label>
+          <input
+            id="firstName"
+            type="text"
+            value={firstName}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Enter your first name"
+          />
+        </div>
 
-        {/* <p className="text-center text-gray-500 text-sm mt-4">
+        {/* Last Name */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="lastName"
+          >
+            Last Name
+          </label>
+          <input
+            id="lastName"
+            type="text"
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Enter your last name"
+          />
+        </div>
+
+        {/* Email */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        {/* WhatsApp */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="whatsapp"
+          >
+            WhatsApp
+          </label>
+          <input
+            id="whatsapp"
+            type="text"
+            value={whatsapp}
+            onChange={(e) => {
+              setWhatsapp(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Enter your WhatsApp number"
+          />
+        </div>
+
+        {/* Phone */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="phone"
+          >
+            Phone
+          </label>
+          <input
+            id="phone"
+            type="text"
+            value={phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Enter your phone number"
+          />
+        </div>
+
+        {/* User Type */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-medium mb-2"
+            htmlFor="userType"
+          >
+            User Type
+          </label>
+          <select
+            id="userType"
+            value={userType}
+            onChange={(e) => {
+              setUserType(e.target.value);
+            }}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          >
+            <option value="admin">Admin</option>
+            <option value="user">User</option>
+          </select>
+        </div>
+
+        {/* Email Verified */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-medium mb-2">
+            <input
+              id="emailVerified"
+              type="checkbox"
+              checked={emailVerified}
+              onChange={(e) => setEmailVerified(e.target.checked)}
+              className="mr-2"
+            />
+            Email Verified
+          </label>
+        </div>
+
+        {/* Disabled */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-medium mb-2">
+            <input
+              id="disabled"
+              type="checkbox"
+              checked={disabled}
+              onChange={(e) => setDisabled(e.target.checked)}
+              className="mr-2"
+            />
+            Disabled
+          </label>
+        </div>
+
+        <button
+          className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+          onClick={handleSubmit}
+        >
+          Register
+        </button>
+
+        <p className="text-center text-gray-500 text-sm mt-4">
           Already have an account? <Link to={"/login"}> Login</Link>
-        </p> */}
+        </p>
       </div>
-      {/* <Routes path="/">
-        <Route path="/login" element={<LoginPages />} />
-      </Routes> */}
     </div>
   );
 }
